f9e3abf1aeadab1483bf374fe41f719229a6b3f4
Juan Camilo Isaza <jkmisaza@gmail.com>
2016-05-31 15:35:13 -0500

    Initial Configuration

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('vimeoTest')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
