2bdb4d90fbff6801cfdf2950e6b3fa7f0b2c4df8
Juan Camilo Isaza <jkmisaza@gmail.com>
2016-06-22 10:07:53 -0500

    Responsive adjustments

--- src/app/components/sidebar/sidebar.directive.js
+++ src/app/components/sidebar/sidebar.directive.js
@@ -6,7 +6,7 @@
     .directive('sideBar', sideBar);
 
   /** @ngInject */
-  function sideBar() {
+  function sideBar($state) {
 
     return {
       restrict: 'E',
@@ -15,7 +15,41 @@
       scope: {
         links: '='
       },
-      link: function() {
+      link: function(scope) {
+
+        var menuBtn = angular.element('.menu'),
+            closeBtn = angular.element('.close-btn'),
+            menuContent = angular.element('.sidebar'),
+            logoContent = angular.element('.logo');
+
+        if (angular.element(window).width() <= 1024) {
+          menuContent.removeClass('active');
+          menuContent.addClass('deactive');
+        }
+
+        angular.element(window).on("resize", function () {
+          if (angular.element(window).width() > 1024) {
+            menuContent.removeClass('deactive');
+            menuContent.addClass('active');
+          } else {
+            menuContent.removeClass('active');
+            menuContent.addClass('deactive');
+          }
+        });
+
+        menuBtn.on("click", function () {
+          menuContent.removeClass('deactive');
+          menuContent.addClass('active');
+        });
+
+        closeBtn.on("click", function () {
+          menuContent.removeClass('active');
+          menuContent.addClass('deactive');
+        });
+
+        logoContent.on("click", function () {
+          $state.go('home.category', {'categoryName': scope.links[0].params.categoryName, 'page': 1 });
+        });
       }
     }
   }
